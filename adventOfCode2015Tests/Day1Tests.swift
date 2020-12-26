cabdb73f880c202eb78edb149746e4ac1f939a37
Graham Berks <graham@fatlazycat.com>
2020-12-26 20:16:56 +0000

    Reformat code

--- adventOfCode2015Tests/Day1Tests.swift
+++ adventOfCode2015Tests/Day1Tests.swift
@@ -9,24 +9,24 @@ import XCTest
 import SwiftHamcrest
 
 class Day1Tests: XCTestCase {
-
+    
     func testPart1() throws {
-      let up = day1.map(String.init).filter{ $0 == "(" }.count
-      let down = day1.map(String.init).filter{ $0 == ")" }.count
-      assertThat(up - down == 280)
-    }
-  
-  func testPart2() throws {
-    var current = 0
-    var floor = 0
-    
-    while (floor != -1) {
-      floor += day1[current] == "(" ? 1 : -1
-      current += 1
+        let up = day1.map(String.init).filter{ $0 == "(" }.count
+        let down = day1.map(String.init).filter{ $0 == ")" }.count
+        assertThat(up - down == 280)
     }
     
-    assertThat(current == 1797)
-  }
+    func testPart2() throws {
+        var current = 0
+        var floor = 0
+        
+        while (floor != -1) {
+            floor += day1[current] == "(" ? 1 : -1
+            current += 1
+        }
+        
+        assertThat(current == 1797)
+    }
 }
 
 extension String {
