9e9ee65a4d92e3bd7c90af86ac0c16e4a1fbfe7d
Graham Berks <graham@fatlazycat.com>
2020-12-26 16:54:58 +0000

    Done Day 1 Part 2

--- adventOfCode2015Tests/Day1Test.swift
+++ adventOfCode2015Tests/Day1Test.swift
@@ -15,10 +15,28 @@ class Day1Test: XCTestCase {
       let down = day1.map(String.init).filter{ $0 == ")" }.count
       assertThat(up - down == 280)
     }
+  
+  func testPart2() throws {
+    var current = 0
+    var floor = 0
+    
+    while (floor != -1) {
+      floor += day1[current] == "(" ? 1 : -1
+      current += 1
+    }
+    
+    assertThat(current == 1797)
+  }
 }
 
 extension String {
     var lines: [String] {
         return self.components(separatedBy: "\n")
+    }
+}
+
+extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
     }
 }
