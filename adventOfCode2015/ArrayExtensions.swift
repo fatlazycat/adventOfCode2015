b722ee0e3998a70dd70789ac3fe209001f1e283a
Graham Berks <graham@fatlazycat.com>
2020-12-27 09:27:47 +0000

    Done Day 2 Part 1

---
+++ adventOfCode2015/ArrayExtensions.swift
@@ -0,0 +1,47 @@
+/**
+ # Tuple-to-array for Swift
+ 
+ By Ethan McTague - January 28, 2020
+ This source code is in the public domain.
+ 
+ ## Example
+ 
+ To convert a tuple of Ints into an array of ints:
+ 
+ ```
+ let ints = (10, 20, 30)
+ let result = [Int].fromTuple(ints)
+ print(result!) // prints Optional([10, 20, 30])
+ ```
+ 
+ */
+
+import Foundation
+
+extension Array {
+  
+  /**
+   Attempt to convert a tuple into an Array.
+   
+   - Parameter tuple: The tuple to try and convert. All members must be of the same type.
+   - Returns: An array of the tuple's values, or `nil` if any tuple members do not match the `Element` type of this array.
+   */
+  static func fromTuple<Tuple>(_ tuple: Tuple) -> [Element]? {
+    let val = Array<Element>.fromTupleOptional(tuple)
+    return val.allSatisfy({ $0 != nil }) ? val.map { $0! } : nil
+  }
+  
+  /**
+   Convert a tuple into an array.
+   
+   - Parameter tuple: The tuple to try and convert.
+   - Returns: An array of the tuple's values, with `nil` for any values that could not be cast to the `Element` type of this array.
+   */
+  static func fromTupleOptional<Tuple>(_ tuple: Tuple) -> [Element?] {
+    return Mirror(reflecting: tuple)
+      .children
+      .filter { child in
+        (child.label ?? "x").allSatisfy { char in ".1234567890".contains(char) }
+      }.map { $0.value as? Element }
+  }
+}
--- adventOfCode2015Tests/Day2Tests.swift
+++ adventOfCode2015Tests/Day2Tests.swift
@@ -2,21 +2,28 @@
 import XCTest
 import SwiftHamcrest
 import Parsing
+import Foundation
 
 class Day2Test: XCTestCase {
+  
+  func testPart1() throws {
+    let parser = Int.parser()
+      .skip(StartsWith("x"))
+      .take(Int.parser())
+      .skip(StartsWith("x"))
+      .take(Int.parser())
+      .map{ (width: $0, height: $1, depth: $2) }
     
-    func testPart1() throws {
-        let parser = Int.parser()
-            .skip(StartsWith("x"))
-            .take(Int.parser())
-            .skip(StartsWith("x"))
-            .take(Int.parser())
-            .map{ (width: $0, height: $1, depth: $2) }
-        
-        let data = day2.lines.map{ parser.parse($0) }
-        
-        print(data)
-        
-        //      assertThat(up - down == 280)
-    }
+    let data = day2.lines.map{ parser.parse($0)! }
+    let areas = data.map { areaNeed($0) }
+    
+    assertThat(areas.reduce(0, +) == 1588178)
+  }
+  
+  func areaNeed(_ dim: (Int, Int, Int)) -> Int {
+    let area = 2*dim.0*dim.2 + 2*dim.0*dim.1 + 2*dim.1*dim.2
+    let lowestValues = Array<Int>.fromTuple(dim)?.sorted().prefix(2)
+    
+    return area + lowestValues![0]*lowestValues![1]
+  }
 }
